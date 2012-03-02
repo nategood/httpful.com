806dd9289aff7118b9d591669676745786bf27a9
Nate Good <nate.good@showclix.com>
2012-03-01 20:40:09 -0500

    moving the docs

--- docs/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Closure"],["c","Exception"],["c","Httpful\\Bootstrap"],["c","Httpful\\Http"],["c","Httpful\\Mime"],["c","Httpful\\Request"],["c","Httpful\\Response"]];
