8c81f3b5bf3832c960be287c76fe4c476550252c
Nathan <nate.good@showclix.com>
2014-03-29 10:21:31 -0400

    updated docs and added latest builds

--- docs.sh
+++ docs.sh
@@ -1,1 +1,1 @@
-php ~/code/apigen/apigen.php --source ~/code/httpful/src --destination ~/code/httpful.com/html/docs --php no
+apigen --source ~/code/httpful/src --destination ~/code/httpful.com/html/docs --php no
--- html/docs/elementlist.js
+++ html/docs/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Httpful\\Bootstrap"],["c","Httpful\\Handlers\\CsvHandler"],["c","Httpful\\Handlers\\FormHandler"],["c","Httpful\\Handlers\\JsonHandler"],["c","Httpful\\Handlers\\MimeHandlerAdapter"],["c","Httpful\\Handlers\\XHtmlHandler"],["c","Httpful\\Handlers\\XmlHandler"],["c","Httpful\\Http"],["c","Httpful\\Httpful"],["c","Httpful\\Mime"],["c","Httpful\\Request"],["c","Httpful\\Response"]];
+ApiGen.elements = [["c","Httpful\\Bootstrap"],["c","Httpful\\Exception\\ConnectionErrorException"],["c","Httpful\\Handlers\\CsvHandler"],["c","Httpful\\Handlers\\FormHandler"],["c","Httpful\\Handlers\\JsonHandler"],["c","Httpful\\Handlers\\MimeHandlerAdapter"],["c","Httpful\\Handlers\\XHtmlHandler"],["c","Httpful\\Handlers\\XmlHandler"],["c","Httpful\\Http"],["c","Httpful\\Httpful"],["c","Httpful\\Mime"],["c","Httpful\\Request"],["c","Httpful\\Response"],["c","Httpful\\Response\\Headers"]];
